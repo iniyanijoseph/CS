23dd10b5e36589ad94c234d09c3092e2213838f7
iniyanijoseph <51347738+iniyanijoseph@users.noreply.github.com>
2020-03-24 11:33:47 -0700

    Clean up code, add in messeger files

--- InterviewManager/client.py
+++
@@ -1,8 +0,0 @@
-import socket
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((socket.gethostname(), 1024))
-totmsg = ""
-while True:
-    msg = s.recv(7)
-    totmsg += msg.decode("utf-8")
-    print(totmsg)
